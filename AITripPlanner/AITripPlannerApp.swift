52ae89bf38f73cdbf646da059e16ca044eb90b8d
daneng1 <engeldb@gmail.com>
2023-06-20 09:10:59 -0700

    Initial Commit

---
+++ AITripPlanner/AITripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITripPlannerApp.swift
+//  AITripPlanner
+//
+//  Created by Dan and Beth Engel on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct AITripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
