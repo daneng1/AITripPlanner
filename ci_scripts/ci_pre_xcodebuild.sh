eb0edd7959a36986698ee20b43030e055b7ac91f
daneng1 <engeldb@gmail.com>
2023-08-04 08:35:55 -0700

    tweak to ci script

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -10,7 +10,7 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $AITripPlanner/ci_scripts || exit 1
+#cd ../ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"OPEN_AI_KEY\":\"%s\",\"UNSPLASH_ACCESS_KEY\":\"%s\"}" "$OPEN_AI_KEY" "$UNSPLASH_ACCESS_KEY" >> ../Secrets.json
